1061c9afebd3c6e582c4f58fc219921d7359c810
Akinorev <gomezgomezveronica@gmail.com>
2017-03-04 12:35:05 +0100

    Added jump scripts, background image and button with jump action.

---
+++ Assets/Scripts/Player Scripts/PlayerJumpScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerJumpScript : MonoBehaviour {
+
+    public static PlayerJumpScript instance;
+
+    private Rigidbody2D myBody;
+    private Animator anim;
+
+    private float forceX, foceY;
+    private float tresholdX = 7f;
+    private float tresholdY = 14f;
+
+    private bool setPower, didJump;
+
+    void Awake ()
+    {
+        MakeInstance();
+    }
+
+    void MakeInstance ()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    public void SetPower (bool SetPower)
+    {
+        this.setPower = SetPower;
+
+        if (setPower)
+        {
+            Debug.Log("We are setting the power");
+        } else
+        {
+            Debug.Log("We are NOT setting the power");
+        }
+    }
+}
